469d58d00eeb6bf74b9eeb712f6b08151adfccfd
PollitoFrito7 <jdelrod04@gmail.com>
2025-10-17 15:28:16 +0200

    hideAction
    last action for pacman donete

--- Practica2/src/es/ucm/fdi/ici/c2526/practica2/grupoYY/mspacman/actions/HideAction.java
+++ Practica2/src/es/ucm/fdi/ici/c2526/practica2/grupoYY/mspacman/actions/HideAction.java
@@ -1,7 +1,12 @@
 package es.ucm.fdi.ici.c2526.practica2.grupoYY.mspacman.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import es.ucm.fdi.ici.Action;
 import es.ucm.fdi.ici.c2526.practica2.grupoYY.PacmanInfo;
+import pacman.game.Constants.DM;
+import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
 
@@ -15,10 +20,46 @@ public class HideAction implements Action {
 	//TODO: Implement
 	@Override
 	public MOVE execute(Game game) {
+		int nextIntersection = -1;
+		double bestMean = Integer.MIN_VALUE;
+		int bestIntersection = -1;
 		for(MOVE m : pi.getCandidateMoves()) {
-			
+			nextIntersection = pi.getMoveToNode().get(m);
+			List<Integer> depth2 = depht2Intersections(game, nextIntersection);
+			for (Integer i : depth2) {
+				double mean = meanDistance(game, i);
+				if (mean > bestMean) {
+					bestMean = mean;
+					bestIntersection = i;
+				}
+
+			}
 		}
-		return null;
+		return game.getNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(), bestIntersection, game.getPacmanLastMoveMade(), DM.PATH);
+	}
+
+	private List<Integer> depht2Intersections(Game game, int intersection) {
+		List<Integer> nextIntersections = new ArrayList<>();
+		for (int i : game.getNeighbouringNodes(intersection)) {
+			while (!game.isJunction(i)) {
+				i = game.getNeighbouringNodes(i, game.getNextMoveAwayFromTarget(i, intersection, DM.PATH))[0];
+			}
+
+			if (i != game.getPacmanCurrentNodeIndex()) {
+				nextIntersections.add(i);
+			}
+		}
+		return nextIntersections;
+	} 
+
+	private double meanDistance(Game game, int node) {
+		double mean = 0;
+
+		for (GHOST ghost : GHOST.values()) {
+			mean += game.getDistance(game.getGhostCurrentNodeIndex(ghost), node, game.getGhostLastMoveMade(ghost), DM.PATH);
+		}
+
+		return mean / 4;
 	}
 
 	@Override
