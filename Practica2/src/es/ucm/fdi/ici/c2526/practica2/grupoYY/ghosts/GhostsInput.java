c5fbf1bd45161cd918f2739910f51bbd74eee259
davidfb1305 <davidf23@ucm.es>
2025-10-18 10:40:04 +0200

    GhostInfo acabado he implementado
    en la teoria funca en la practica dios nos pille confesados

--- Practica2/src/es/ucm/fdi/ici/c2526/practica2/grupoYY/ghosts/GhostsInput.java
+++ Practica2/src/es/ucm/fdi/ici/c2526/practica2/grupoYY/ghosts/GhostsInput.java
@@ -1,11 +1,19 @@
 package es.ucm.fdi.ici.c2526.practica2.grupoYY.ghosts;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 import es.ucm.fdi.ici.Input;
+import es.ucm.fdi.ici.c2526.practica2.grupoYY.GhostInfo;
 import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Game;
 
 public class GhostsInput extends Input {
+
+	
 
 	private boolean BLINKYedible;
 	private boolean INKYedible;
@@ -13,10 +21,57 @@ public class GhostsInput extends Input {
 	private boolean SUEedible;
 	private double minPacmanDistancePPill;
 	
-	public GhostsInput(Game game) {
+	private Map<GHOST,Integer> distanceFromGhostToPacman;
+	private Map<GHOST,Integer> distanceFromPacmanToGhost;
+	private Map<GHOST,Map<GHOST,Integer>> distanceFromGhostToGhost;
+	private Map<GHOST,GHOST> shieldGhost;
+	private List<GHOST> ghostPriority;
+	
+	private GhostInfo gi;
+	
+	public GhostsInput(Game game,GhostInfo gi) {
 		super(game);
+		this.gi = gi;
 	}
-
+	private void parseDistanceFromGhostToPacman() {
+		distanceFromGhostToPacman = new HashMap<>();
+		for(GHOST g : GHOST.values()) {
+			int auxDistance = game.getApproximateShortestPathDistance(game.getGhostCurrentNodeIndex(g) , game.getPacmanCurrentNodeIndex(), game.getGhostLastMoveMade(g));
+			distanceFromGhostToPacman.put(g, auxDistance);
+		}
+		this.gi.setFromGhostToPacMan(distanceFromGhostToPacman);
+	}
+	
+	private void parseFromPacManToGhost() {
+		distanceFromPacmanToGhost = new HashMap<>();
+		
+		for(GHOST g : GHOST.values()) {
+			int auxDistance = game.getApproximateShortestPathDistance(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(g),game.getPacmanLastMoveMade());
+			distanceFromPacmanToGhost.put(g, auxDistance);
+		}
+		this.gi.setFromPacmanToGhost(distanceFromPacmanToGhost);
+	}
+	
+	private void parseDistanceGhostToGhost() {
+		distanceFromGhostToGhost= new HashMap<>();
+		for(GHOST g : GHOST.values()) {
+			Map<GHOST,Integer> auxMap = new HashMap<>();
+			for(GHOST otherghost : GHOST.values()) {
+				int auxdistance = game.getApproximateShortestPathDistance( game.getGhostCurrentNodeIndex(g), game.getGhostCurrentNodeIndex(otherghost),game.getGhostLastMoveMade(g));
+				auxMap.put(otherghost, auxdistance);
+			}
+			distanceFromGhostToGhost.put(g, auxMap);
+			this.gi.setDistanceFromGhostToGhost(distanceFromGhostToGhost);
+		}
+	}
+	
+	private void parseGhostPriority() {
+		TreeMap<Integer,GHOST>auxMap = new TreeMap<>();
+		distanceFromGhostToPacman.forEach((key,element) -> auxMap.put(element, key));
+		auxMap.forEach((key,element)-> ghostPriority.add(element));
+		
+		this.gi.setGhostPriority(ghostPriority);
+	}
 	@Override
 	public void parseInput() {
 		this.BLINKYedible = game.isGhostEdible(GHOST.BLINKY);
@@ -30,6 +85,11 @@ public class GhostsInput extends Input {
 			double distance = game.getDistance(pacman, ppill, DM.PATH);
 			this.minPacmanDistancePPill = Math.min(distance, this.minPacmanDistancePPill);
 		}
+		parseDistanceFromGhostToPacman();
+		parseFromPacManToGhost();
+		parseDistanceGhostToGhost();
+		 parseGhostPriority();
+		
 	}
 
 	public boolean isBLINKYedible() {
