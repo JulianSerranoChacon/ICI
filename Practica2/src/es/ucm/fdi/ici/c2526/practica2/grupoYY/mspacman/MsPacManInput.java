27ecdcca80f2469c60cc59757aa1660402ec7ce6
PollitoFrito7 <jdelrod04@gmail.com>
2025-10-19 17:42:28 +0200

    some tunning
    still inconsistent

--- Practica2/src/es/ucm/fdi/ici/c2526/practica2/grupoYY/mspacman/MsPacManInput.java
+++ Practica2/src/es/ucm/fdi/ici/c2526/practica2/grupoYY/mspacman/MsPacManInput.java
@@ -28,7 +28,7 @@ public class MsPacManInput extends Input {
 	private List<MOVE> candidateMoves;
 	private int closestPPill;
 	private double distanceToPPill;
-	private final double dangerDistance = 20; //tentative, subject to change 
+	private final double dangerDistance = 30; //tentative, subject to change 
 	private final double hideDistance = 30; //tentative, subject to change 
 	
 	//Info for actions
